2060e49934cfa760c24da21f53b2b8024c018aec
Nishant <nishant2024rde@gmail.com>
2025-11-17 08:27:43 +0530

    transferring_files to main_js and cleaning the Enigma.js

--- Main_Machine/Enigma.js
+++ Main_Machine/Enigma.js
@@ -1,19 +1,13 @@
 export class Enigma{
-    // constructor(kb, pb, r1, r2, r3, rf){
+    
     constructor(kb, pb, rotors, rf){
         this.kb = kb;
         this.pb = pb;
-        // this.r1 = r1;
-        // this.r2 = r2;
-        // this.r3 = r3;
         this.rf = rf;
         this.rotors = rotors
 
     }
     set_key(key){
-        // this.r1.rotate_to_letter(key[0])
-        // this.r2.rotate_to_letter(key[1])
-        // this.r3.rotate_to_letter(key[2])
         for ( let i = 0; i < this.rotors.length; i++){
             this.rotors[i].rotate_to_letter(key[i])
         }
@@ -28,26 +22,10 @@ export class Enigma{
             }
         }
 
-        // if (this.r2.left[0] === this.r2.notch && this.r1.left[0] === this.r1.notch){
-        //     this.r1.rotate()
-        //     this.r2.rotate()
-        //     this.r3.rotate()
-        // }
-
-        // else if (this.r3.left[0] === this.r3.notch){
-        //     this.r2.rotate()
-        //     this.r3.rotate()
-        // }
-
-        // else{
-        //     this.r3.rotate()
-        // }
-
+        
         let signal = this.kb.forward(letter);
         signal = this.pb.forward(signal);
-        // signal = this.r3.forward(signal);
-        // signal = this.r2.forward(signal);
-        // signal = this.r1.forward(signal);
+        
         for (let i = this.rotors.length - 1; i >= 0; i--){
             signal = this.rotors[i].forward(signal);
         }
@@ -55,9 +33,7 @@ export class Enigma{
         for (let i = 0; i < this.rotors.length; i++){
             signal = this.rotors[i].backward(signal);
         }
-        // signal = this.r1.backward(signal);
-        // signal = this.r2.backward(signal);
-        // signal = this.r3.backward(signal);
+        
         signal = this.pb.backward(signal);
         letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[signal];
         return letter;
