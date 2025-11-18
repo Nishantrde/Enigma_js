1142b230d5b110991c05c4b249405a2c6bacb697
Nishant <nishant2024rde@gmail.com>
2025-11-18 14:54:28 +0530

    upgrade----app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// app.js
-// import { Keyboard } from './Main_Machine/Keyboard.js'; // <- named import
-// import express from 'express';
-// import path from 'path';
-const { Keyboard } = require('./Main_Machine/Keyboard.js'); // <- named import
+const { Keyboard } = require('./Main_Machine/Keyboard.js'); // <- named import of the class
 const express = require('express');
 const path = require('path');
 const app = express();
-// const keyboard = new Keyboard(); // instantiate
+
+
+const keyboard = new Keyboard();
 
 app.get('/', (req, res) => {
-  res.send('Hello World — forward(A) => ' + Keyboard.forward('A'));
+    const output = keyboard.forward('Z');
+    res.send('Hello World — forward(A) => ' + output);
 });
-app.get('/enigma', (req, res) => {
-  res.sendFile(path.join(  __dirname, 'templates', 'index.html'));
-  res.json({ message: 'Enigma endpoint' });
-})
 
-app.listen(3000, () => {
-  console.log('server running on port http://localhost:3000');
+app.get('/enigma', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server running on port http://localhost:${PORT}`);
+});
+
